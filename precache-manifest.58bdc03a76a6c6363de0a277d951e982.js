15f4d09e97e84c6df74c02e0211c535399e2493f
Khazady <seakingtourist@gmail.com>
2020-07-09 02:57:34 +0300

    Updates

---
+++ precache-manifest.58bdc03a76a6c6363de0a277d951e982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ba144f8fb7bf3d5d93e9904b86c8d1",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "bd86f762f39020809a7e",
+    "url": "/Counter/static/css/main.c4fcf467.chunk.css"
+  },
+  {
+    "revision": "b50820a83edbbd54e471",
+    "url": "/Counter/static/js/2.3368ebc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.3368ebc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd86f762f39020809a7e",
+    "url": "/Counter/static/js/main.4924ab17.chunk.js"
+  },
+  {
+    "revision": "64ebb70e29fb0dcf293f",
+    "url": "/Counter/static/js/runtime-main.f869fd43.js"
+  }
+]);
